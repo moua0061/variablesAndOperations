dd1a7e5cc8533e685e214b2d06b6ce41f4c5638f
Lia <moua0061@yahoo.com>
2022-08-13 14:15:14 -0400

    completed first

---
+++ ArraysAndMethods.java
@@ -0,0 +1,37 @@
+public class ArraysAndMethods {
+
+    public static void main(String[] args) {
+
+        // create an array wth the following values: 1,5,2,8,13, 6
+        int[] arr = new int[6];
+        arr[0] = 1;
+        arr[1] = 5;
+        arr[2] = 2;
+        arr[3] = 8;
+        arr[4] = 13;
+        arr[5] = 6;
+
+        // print out the first element in the array
+
+        // print out the element in the array at position 6, what happens?
+
+        // print out the element in the array at position -1. what happens?
+
+        // write a traditional for loop that prints out each element in the array
+
+        // write an enhanced for loop that prints out each element in the array
+
+        // create a new variable called sum ,write a loop that adds each element in the
+        // array to the sum
+
+        // create a new variable called average and assign the average value of the
+        // array to it
+
+        // write an enhanced for loop that prints out each number in the array only if
+        // the number is odd
+
+        // create an array that contains the values "sam, sally, thomas, and robert"
+        // calculate the num of all the letters in the new array
+
+    }
+}
