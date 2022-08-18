818b0fea898acfd7af7011780b1248093fe92422
Lia <moua0061@yahoo.com>
2022-08-17 21:56:23 -0400

    completed

--- Lists.java
+++ Lists.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lists {
 
     public static void main(String[] args) {
 
+        ArrayList<String> sports = new ArrayList<>();
+        sports.add("football");
+        sports.add("basketball");
+        sports.add("volleyball");
+        sports.remove(0);
+
+        System.out.println(sports);
+
+        // for (int i = 0; i < sports.size(); i++) {
+        // System.out.println(sports.get(i));
+        // }
+
+        // for (String sport : sports) {
+        // System.out.println(sport);
+        // }
+
+        // don't forget to import List from java.util
+        List<Integer> test = new ArrayList<Integer>();
+        test.add(3);
+        test.add(5);
+
+        System.out.println(test);
+        System.out.println(test.isEmpty());
     }
 }
