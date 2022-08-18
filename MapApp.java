81796cc75b171ca05fb35c950fa3633998486af3
Lia <moua0061@yahoo.com>
2022-08-17 23:19:24 -0400

    hash map exercise

---
+++ MapApp.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MapApp {
+    static Scanner scan = new Scanner(System.in);
+    static Map<String, String> phoneBook = new HashMap<>();
+
+    public static void main(String[] args) {
+
+    }
+}
