9cd0387c9489fd86ea19b908d5758badf49d3c46
Lia <moua0061@yahoo.com>
2022-08-17 21:32:48 -0400

    playing with toString()

--- StringAndStringBuilder.java
+++ StringAndStringBuilder.java
@@ -9,6 +9,25 @@ public class StringAndStringBuilder {
         System.out.println(testing.charAt(3));
         testing.deleteCharAt(2);
         System.out.println(testing);
+        testing.delete(2, 4);
+        System.out.println(testing);
+        System.out.println(testing.indexOf("o"));
+        // System.out.println(testing);
+        testing.replace(2, 4, "hi ");
+        System.out.println(testing);
+
+        testing.reverse();
+        System.out.println(testing);
+
+        Integer num = 3;
+        // cannot invoke toString() on primitive data
+        System.out.println(num.toString());
+
+        StringBuilder results = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            results.append("nyob zoo, ");
+        }
+        System.out.println(results);
 
     }
 }
