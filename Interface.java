d2c1961e9e8b397ca63b8c55b5b25a5f304895b4
Lia <moua0061@yahoo.com>
2022-08-28 19:12:27 -0400

    added interface exercise

---
+++ Interface.java
@@ -0,0 +1,7 @@
+public interface Interface {
+    // when a class implements an interface, it must
+
+    // an interface only has abstract method(s)
+    // abstract methods does not include brackets {}
+    public void info(String info);
+}
