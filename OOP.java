dfd7e18623526ed0de05853328937efb1adda2a8
Lia <moua0061@yahoo.com>
2022-09-01 21:37:57 -0400

    oop

--- OOP.java
+++ OOP.java
@@ -1,6 +1,15 @@
 public class OOP {
     /*
      * OOP
+     * 4 pillars of OOP:
+     * 1. abstraction
+     * 2. encapsulation
+     * 3. inheritance
+     * 4. polymorphism
+     * 
+     * class : is like a blueprint
+     * object : the actual product created from the blueprint
+     * an object is an instance of a class
      */
 
     public static void main(String[] args) {
